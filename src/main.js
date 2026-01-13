5ebff89ec6ee178de87637a21de7614636070fd4
parham <parhamramin1386@gmail.com>
2026-01-13 13:36:38 +0400

    #9 Navbar Change ,Home page Gallery

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ const mymenu = document.getElementById("mymenu");
 const closeBtn = document.getElementById("closeBtn");
 const overlay = document.getElementById("overlay");
 
-
 const toggleMenu = () => {
   mymenu.classList.toggle("-right-full");
   mymenu.classList.toggle("right-0");
@@ -20,15 +19,6 @@ mybtn.addEventListener("click", toggleMenu);
 if (closeBtn) {
   closeBtn.addEventListener("click", toggleMenu);
 }
-
-
-
-// const menuLinks = mymenu.querySelectorAll("a");
-// menuLinks.forEach((link) => {
-//   link.addEventListener("click", toggleMenu);
-// });
-
-
 
 // sticky navbar on scroll && glassy effect
 
@@ -73,5 +63,82 @@ btnback.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
+  });
+});
+
+////Image slider Automatic
+
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll(".carousel-item img");
+  const dots = document.querySelectorAll(".dot");
+  const prevBtn = document.querySelector("#prevBtn");
+  const nextBtn = document.querySelector("#nextBtn");
+  let currentIndex = 0;
+  let slideInterval;
+
+  function updateSlider(index) {
+    slides.forEach((img) => {
+      img.classList.remove("opacity-100");
+      img.classList.add("opacity-0");
+    });
+    dots.forEach((dot) => {
+      dot.className =
+        "dot w-2 h-2 rounded-full bg-white/20 transition-all duration-300";
+    });
+
+    currentIndex = index;
+    slides[currentIndex].classList.remove("opacity-0");
+    slides[currentIndex].classList.add("opacity-100");
+    dots[currentIndex].className =
+      "dot w-6 h-2 rounded-full bg-white transition-all duration-300";
+  }
+
+  function nextSlide() {
+    let next = (currentIndex + 1) % slides.length;
+    updateSlider(next);
+  }
+
+  function prevSlide() {
+    let prev = (currentIndex - 1 + slides.length) % slides.length;
+    updateSlider(prev);
+  }
+
+  nextBtn.addEventListener("click", () => {
+    nextSlide();
+    resetTimer();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    prevSlide();
+    resetTimer();
+  });
+
+  function startTimer() {
+    slideInterval = setInterval(nextSlide, 4000);
+  }
+
+  function resetTimer() {
+    clearInterval(slideInterval);
+    startTimer();
+  }
+
+  startTimer();
+});
+
+///FAQ
+
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach((question) => {
+  question.addEventListener("click", function () {
+    const arrow = this.querySelector(".arrow");
+    const answer = this.nextElementSibling;
+    arrow.classList.toggle("rotate-180");
+
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
   });
 });
